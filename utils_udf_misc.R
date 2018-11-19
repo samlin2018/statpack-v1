f20469fdeeef18fc91c7eff0f7bc39079b91742f
Sam Lin <sam.lin@lb.thecore.group>
2018-11-19 11:01:23 -0600

    first update

---
+++ utils_udf_misc.R
@@ -0,0 +1,9 @@
+wordswitch <- function(str) {
+  switch(str,
+         conf = "confidence level",
+         pow = "power",
+         conv = "test - conversion rate",
+         size = "sample size",
+         n2 = " of control",
+         n1 = " of test")
+}
